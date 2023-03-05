a2f59590382bd5ddd7fca9a74ed65dd656537993
hajimism <h_nakagawa@yumemi.co.jp>
2023-03-05 12:44:57 +0900

    feat: add button

---
+++ components/ui/Button/Button.stories.tsx
@@ -0,0 +1,75 @@
+import { StoryObj, Meta } from "@storybook/react";
+import { Loader2, Mail } from "lucide-react";
+
+import { Button } from ".";
+
+const meta: Meta<typeof Button> = {
+  title: "components/Button",
+  component: Button,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: "Button",
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: "destructive",
+    children: "Destructive",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+    children: "Ghost",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: "outline",
+    children: "Outline",
+  },
+};
+
+export const Subtle: Story = {
+  args: {
+    variant: "subtle",
+    children: "Subtle",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: "link",
+    children: "Link",
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    children: (
+      <>
+        <Mail className="mr-2 h-4 w-4" /> Login with Email
+      </>
+    ),
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    children: (
+      <>
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        Please wait
+      </>
+    ),
+  },
+};
